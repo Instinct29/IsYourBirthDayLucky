695896fd3bbf87474b526b7068b3eaed3348b548
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2021-11-10 14:11:01 +0530

    Js Done.
    some Important concepts in this =>
    1.typeof ->tells the type if its string or someother Data type
    2.replaceAll("-","") -> line29 to replace any character in a string
    3.characterAt(i)->line 32
    4.Typecasting eg Number() ->linne32

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const birthdate = document.querySelector("#date-of-birth");
 const luckynumber = document.querySelector("#lucky-number");
 const checknumberbutton = document.querySelector(".check-number");
+const resultbox = document.querySelector("#result");
+checknumberbutton.addEventListener('click', checkBirthdateIsLucky);
 
-checknumberbutton.addEventListener('click',function getvalues(){
-    console.log(birthdate.value,luckynumber.value);
-})
+function comparevalues(sum,luckynumber){
+     if(sum%luckynumber===0){
+        //  resultbox.value="yayah Your BirthDay is Lucky 😊✔️"
+        resultbox.innerText="yayah Your BirthDay is Lucky 😊✔️"
+     }
+     else{
+        //  resultbox.value="Not lucky!lets create our own luck 🔥"
+        resultbox.innerText="Not lucky!lets create our own luck 🔥"
+    }
+}
+function checkBirthdateIsLucky(){
+    const date = birthdate.value;
+    const sum = calculatesum(date);
+    if(sum&&luckynumber.value)
+    comparevalues(sum,luckynumber.value)
+    else{
+        // resultbox.value = "please enter both fields 👽 ❓  "
+        resultbox.innerText="please enter both fields 👽 ❓  "
+    }
+
+}
+function calculatesum(date){
+    date = date.replaceAll("-","")
+    let sum = 0;
+    for(var i = 0 ; i<date.length; i++){
+    sum = sum + Number(date.charAt(i))
+    }
+    return sum;
+}
+
+
+   
